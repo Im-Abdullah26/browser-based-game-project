e60bd1f0f8608336176f19c28e375402abf3c034
abdullah <abdullamsuleman@gmail.com>
2026-06-29 00:38:15 +0300

    update 6

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const gameBoard = document.getElementById("game-board")
+const gameBoard = document.getElementById("game-board");
 
 for (let i = 1; i <= 16; i++) {
-    
-    const card = document.createElement("div")
 
-    card.className = "card"
-    
-    gameBoard.appendChild(card)
+    const card = document.createElement("div");
+
+    card.className = "card";
+
+    card.addEventListener("click", function () {
+
+        card.textContent = "⭐"
+
+        card.style.backgroundColor =  "yellow"
+
+    });
+
+    gameBoard.appendChild(card);
+
 }

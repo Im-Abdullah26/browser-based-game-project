a566821344d9cb4358f7cea601b9bf84e5feeb67
abdullah <abdullamsuleman@gmail.com>
2026-06-29 16:45:46 +0300

    Nabila added event.target

--- app.js
+++ app.js
@@ -18,25 +18,55 @@ for (let i = 0; i < 16; i++) {
     const card = document.createElement("div")
 
     card.className = "card"
-
-    card.textContent = emojis[i]
-
-    gameBoard.appendChild(card)
-
-
-}
-
-
-
-for (let i = 0; i < 16; i++) { //suffle loop//
-
-    let randomIndex = Math.floor(Math.random() * emojis.length);
-
-    let temp = emojis[i]
-
-    emojis[i] = emojis[randomIndex]
+    card.id= i
     
-    emojis[randomIndex] = temp
+    gameBoard.appendChild(card)
+    
+    card.addEventListener( "click", function(event){
+        card.style.backgroundColor= 'yellow'
+        // gets the card's id
+        console.log(event.target.id)
+        // get the card element
+        console.log(event.target)
+        event.target.textContent = emojis[event.target.id]
+        // card.textContent = emojis[i]
+    })
+
 
 }
+
+
+
+// for (let i = 0; i < 16; i++) { //suffle loop//
+
+//     let random = Math.floor(Math.random() * 16)
+
+//     let temp = emojis[i]
+
+//     emojis[i] = emojis[random]
+    
+//     emojis[random] = temp
+
+
+// }
+
+
 console.log(emojis)
+
+let time = 30
+
+const runTimer = setInterval(function(){
+       
+    time = time - 1
+       
+        // console.log('time left: ', time)
+    }, 1000)
+
+setTimeout(function() {
+
+    clearInterval(runTimer)
+    console.log('Time is Up')
+}, 30000)
+
+
+
